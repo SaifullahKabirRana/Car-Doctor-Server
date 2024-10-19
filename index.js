5917118e3e3d77c9f0293b5af7946fd398c20051
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2024-10-20 03:27:46 +0600

    Send token server to client and client to the server side

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true,
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xmhoqrm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -34,8 +39,13 @@ async function run() {
     app.post('/jwt', async (req, res) => {
       const user = req.body;
       console.log(user);
-      const token = jwt.sign(user, 'secret', {expiresIn: '1h'})
-      res.send(token);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+      res
+      .cookie('token', token, {
+        httpOnly: true,
+        secure: false,
+      })
+      .send({success: true});
       
     })
 
@@ -62,6 +72,7 @@ async function run() {
     // checkout
 
     app.get('/checkout', async (req, res) => {
+      console.log('tok tok token', req.cookies.token);
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
