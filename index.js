f7792887c9f22348f811da8da25c0964b9623334
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2024-10-23 02:56:09 +0600

    JWT token verify and set 401, 403

--- index.js
+++ index.js
@@ -28,6 +28,32 @@ const client = new MongoClient(uri, {
   }
 });
 
+// middlewares
+const logger = async (req, res, next) => {
+  console.log('called:', req.host, req.originalUrl);
+  next();
+}
+
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+  console.log('value of token in middleware', token);
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    //error
+    if (err) {
+      console.log(err);
+      return res.status(401).send({ message: 'unauthorized access' });
+    }
+    // if token is valid then it would be decoded
+    console.log('value in the token:', decoded);
+    req.user = decoded;
+    next()
+  })
+
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -37,7 +63,7 @@ async function run() {
     const checkoutCollection = client.db('carDoctor').collection('checkout')
 
     // auth related api 
-    app.post('/jwt', async (req, res) => {
+    app.post('/jwt', logger, async (req, res) => {
       const user = req.body;
       console.log(user);
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
@@ -54,7 +80,7 @@ async function run() {
     })
 
     // services related api
-    app.get('/services', async (req, res) => {
+    app.get('/services', logger, async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
       res.send(result);
@@ -74,9 +100,13 @@ async function run() {
     })
 
     // checkout
+    app.get('/checkout', logger, verifyToken, async (req, res) => {
+      // console.log('tik tok token:', req.cookies.token);
+      console.log('user in the valid token:', req.user);
+      if(req.query.email !== req.user.email){
+        return res.status(403).send({message: 'forbidden access'})
+      }
 
-    app.get('/checkout', async (req, res) => {
-      console.log('tik tok token', req.cookies.token);
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
